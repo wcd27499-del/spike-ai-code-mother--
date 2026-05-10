09a087de36dffa4227c35a63c7e1c14559777884
吴传德 <333750705@qq.com>
2026-05-11 02:46:44 +0800

    用户模块

--- src/main/java/com/spike/spikeaicodemother/SpikeAiCodeMotherApplication.java
+++ src/main/java/com/spike/spikeaicodemother/SpikeAiCodeMotherApplication.java
@@ -1,9 +1,11 @@
 package com.spike.spikeaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@MapperScan("com.spike.spikeaicodemother.genresult.mapper")
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
 public class SpikeAiCodeMotherApplication {
